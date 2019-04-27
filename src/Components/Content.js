8391da1571e0b2e0b91f628095840f3592432c03
Zayne  Berndt <berndt.zayne@gmail.com>
2019-04-28 00:13:19 +0100

    added layout

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,20 +1,72 @@
 import React from "react";
 import "../assets/Content.css";
 
-
-
-
 class CarouselImg extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div class="grid-container">
-          <img  src={require('../assets/img/cake3.jpg')} width={500} height={400} />
+        <div className="row">
+          <div id="col0" className="column" />
+          <div className="column">Column</div>
+          <div id="col0" className="column" />
         </div>
-        <div className="grid-container2">
-          <div className="content-text">MY <span style={{color: "#F889A2"}}>CAKES</span> ARE <br></br> HANDCRAFTED<br></br> WITH LOVE.</div> 
-          <div className="content-parra"><p>Ipsum loewm somehting goes here for cakes and <br/>Ipsum loewm somehting goes here <br/>Ipsum loewm somehting goes here for cakes <br/>Ipsum loewm somehting goes here for</p></div>
+        <div className="row">
+          <div id="col0" className="column" />
+          <div id="col1" className="column">
+          <div className="content-text">
+          MY <span style={{ color: "#F889A2" }}>CAKES</span> ARE <br />{" "}
+          HANDCRAFTED<br /> WITH LOVE.
         </div>
+        <div className="content-parra">
+          <p>
+            Ipsum loewm somehting goes here for cakes and <br />
+            Ipsum loewm somehting goes here <br/>
+            Ipsum loewm somehting goes here for cakes <br />
+            Ipsum loewm somehting goes here for
+          </p>
+        </div>
+          </div>
+          <div id="col2" className="column">
+            Column
+          </div>
+          <div id="col0" className="column" />
+        </div>
+        <div className="row">
+          <div id="col0" className="column" />
+          <div id="col3" className="column">
+            Column
+          </div>
+          <div id="col4" className="column">
+            Column
+          </div>
+          <div id="col5" className="column">
+            Column
+          </div>
+          <div id="col0" className="column" />
+        </div>
+        <div className="row">
+          <div id="col0" className="column"/>
+          <div id="col6" className="column">
+            Column
+          </div>
+          <div id="col7" className="column">
+            Column
+          </div>
+          <div id="col8" className="column">
+            Column
+          </div>
+          <div id="col9" className="column">
+          Column
+          </div>
+          <div id="col10" className="column">
+          Column
+        </div>
+          <div id="col0" className="column" />
+        </div>
+        <div class="footer">
+          <p>Footer</p>
+        </div>
+      
       </React.Fragment>
     );
   }
